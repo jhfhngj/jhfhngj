afa9caf751a84fe3619dd330c713adb241be75ba
jhfhngj <99299697+jhfhngj@users.noreply.github.com>
2022-03-12 18:18:20 -0800

    Create App.cs

---
+++ App.cs
@@ -0,0 +1,7 @@
+Console.WriteLine(value: "Scratch game downloader v12.34 beta release. What do you want the installer to do? [I] Install [S] Visit Scratch");
+if Console.Beep
+    while (true)
+        Console.WriteLine(value: "Failed! Please press [R] to restart.");
+            while (true)
+    Console.WriteLine(value: "Restarted! Install or visit Scratch? If there is no I do not know Then: Close the setup.")
+    Console.Beep
